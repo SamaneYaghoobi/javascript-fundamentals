749baf7048a2a7ac621909ba3f47fc3bdfb915dd
Samane <samaneyaghoobi1996@gmail.com>
2022-05-23 08:50:02 +0430

    add starter files

---
+++ script.js
@@ -0,0 +1,84 @@
+/* let myFirstName = "Sahar";
+
+console.log("Samane");
+console.log("Samane");
+console.log(myFirstName);
+console.log(myFirstName);
+
+let days = 4; */
+
+// NUMBER
+
+// let numberType = 4;
+
+// console.log(typeof numberType);
+
+// // STRING
+// let stringType = "4";
+// console.log(typeof stringType);
+
+// // BOOLEAN
+// let booleanType = false;
+// console.log(typeof booleanType);
+
+// // UNDEFINED
+// let undefinedType;
+// console.log(typeof undefinedType);
+
+// // NULL
+
+// let nullType = null;
+// console.log(typeof nullType)
+
+// stringType = true;
+// console.log(typeof stringType)
+
+// let firstName = "samane";
+// firstName = 'sahar';
+
+// var firstName1 = "sahar"
+// firstName1 = "ali";
+
+// const birthDate;
+
+// const now = 2023;
+// let myAge = now - 1994;
+// let saharAge = now - 1994;
+
+// const myFullName = "samane" + " " + "Yaghoobi";
+
+// const isSamaneOlder = saharAge === myAge;
+
+// const elnazWeight = 65;
+// const elnazHeight = 1.7;
+
+// const elnazBMI = elnazWeight / elnazHeight ** 2;
+
+// console.log(elnazBMI);
+
+// let elnazStatus;
+
+// if (elnazBMI < 25) {
+//   elnazStatus = "healthy";
+//   console.log("Elnaz is " + elnazStatus);
+// } else {
+//   elnazStatus = "overweight";
+//   console.log("Elnaz is " + elnazStatus);
+// }
+
+const day = "sunday";
+
+switch (day) {
+  case "monday":
+    console.log("plan course structure");
+    break;
+  case "sunday":
+    console.log("prepare content");
+    break;
+  case "thursday":
+  case "friday":
+    console.log("record videos");
+    break;
+  default:
+    console.log("not a valid day");
+}
